bb56e5075dc1a5d2ec5107b2f6d649ce9dac9d7a
Behrouz Hasanebygi <behrouz.hasanbeygi@smartly.io>
2022-08-31 15:45:12 +0300

    add test for path specification too

--- pkg/envoy/route_test.go
+++ pkg/envoy/route_test.go
@@ -12,14 +12,22 @@ func notIsType(a, b interface{}) bool {
 	return reflect.TypeOf(a) != reflect.TypeOf(b)
 }
 
+func notIsPath(a, b string) bool {
+	return a != b
+}
+
 func TestPrefixStringPath(t *testing.T) {
 	ingress := newIngress("foo.app.com", "foo.cluster.com", pathvars{"path": "/", "pathType": networkingv1.PathTypePrefix})
 	c := translateIngresses([]networkingv1.Ingress{ingress})
 
 	pathtype := c.VirtualHosts[0].Routes[0].Route.GetPathSpecifier()
+	pathroute := c.VirtualHosts[0].Routes[0].Route.GetPrefix()
 
 	if notIsType(pathtype, &envoy_route_v3.RouteMatch_Prefix{}) {
-		t.Errorf("expected pathType for foo.app.com ingress , was %s", reflect.TypeOf(pathtype))
+		t.Errorf("expected PathType for foo.app.com ingress , was %s", reflect.TypeOf(pathtype))
+	}
+	if notIsPath(pathroute, ingress.Spec.Rules[0].HTTP.Paths[0].Path) {
+		t.Errorf("expected PathRoute for foo.app.com ingress , was %s", ingress.Spec.Rules[0].HTTP.Paths[0].Path)
 	}
 }
 
@@ -28,9 +36,13 @@ func TestPrefixPath(t *testing.T) {
 	c := translateIngresses([]networkingv1.Ingress{ingress})
 
 	pathtype := c.VirtualHosts[0].Routes[0].Route.GetPathSpecifier()
+	pathroute := c.VirtualHosts[0].Routes[0].Route.GetPathSeparatedPrefix()
 
 	if notIsType(pathtype, &envoy_route_v3.RouteMatch_PathSeparatedPrefix{}) {
-		t.Errorf("expected pathType for foo.app.com ingress , was %s", reflect.TypeOf(pathtype))
+		t.Errorf("expected PathType for foo.app.com ingress , was %s", reflect.TypeOf(pathtype))
+	}
+	if notIsPath(pathroute, ingress.Spec.Rules[0].HTTP.Paths[0].Path) {
+		t.Errorf("expected PathRoute for foo.app.com ingress , was %s", ingress.Spec.Rules[0].HTTP.Paths[0].Path)
 	}
 }
 
@@ -39,9 +51,13 @@ func TestRegexPath(t *testing.T) {
 	c := translateIngresses([]networkingv1.Ingress{ingress})
 
 	pathtype := c.VirtualHosts[0].Routes[0].Route.GetPathSpecifier()
+	pathroute := c.VirtualHosts[0].Routes[0].Route.GetSafeRegex().GetRegex()
 
 	if notIsType(pathtype, &envoy_route_v3.RouteMatch_SafeRegex{}) {
-		t.Errorf("expected pathType for foo.app.com ingress , was %s", reflect.TypeOf(pathtype))
+		t.Errorf("expected PathType for foo.app.com ingress , was %s", reflect.TypeOf(pathtype))
+	}
+	if notIsPath(pathroute, "^/foo/.*") {
+		t.Errorf("expected PathRoute for foo.app.com ingress , was %s", ingress.Spec.Rules[0].HTTP.Paths[0].Path)
 	}
 }
 
@@ -50,8 +66,12 @@ func TestExactPath(t *testing.T) {
 	c := translateIngresses([]networkingv1.Ingress{ingress})
 
 	pathtype := c.VirtualHosts[0].Routes[0].Route.GetPathSpecifier()
+	pathroute := c.VirtualHosts[0].Routes[0].Route.GetPath()
 
 	if notIsType(pathtype, &envoy_route_v3.RouteMatch_Path{}) {
-		t.Errorf("expected pathType for foo.app.com ingress , was %s", reflect.TypeOf(pathtype))
+		t.Errorf("expected PathType for foo.app.com ingress , was %s", reflect.TypeOf(pathtype))
+	}
+	if notIsPath(pathroute, ingress.Spec.Rules[0].HTTP.Paths[0].Path) {
+		t.Errorf("expected PathRoute for foo.app.com ingress , was %s", ingress.Spec.Rules[0].HTTP.Paths[0].Path)
 	}
 }
